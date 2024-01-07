149081b0c7fe08fd0ae9e237e34cad48cddf7fbb
Bayu Febriyono Putra <92196679+BayuFebriyono@users.noreply.github.com>
2024-01-07 08:03:52 +0700

    add fitur add customer

--- app/Livewire/Admin/MasterData/Customer.php
+++ app/Livewire/Admin/MasterData/Customer.php
@@ -16,14 +16,48 @@ class Customer extends Component
 
     public $active = 'customer';
     public $search = '';
+    public $isAddData = false;
+
+    public $nama = '';
+    public $alamat = '';
+
     public function render()
     {
-        $customers = ModelsCustomer::where('nama', 'like', '%'.$this->search.'%')
+        $customers = ModelsCustomer::where('nama', 'like', '%' . $this->search . '%')
             ->orWhere('alamat', 'like', '%' . $this->search . '%')
             ->paginate(10);
 
-        return view('livewire.admin.master-data.customer',[
+        return view('livewire.admin.master-data.customer', [
             'customers' => $customers
         ]);
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'nama' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        ModelsCustomer::create([
+            'nama' => $this->nama,
+            'alamat' => $this->alamat
+        ]);
+
+        session()->flash('success', 'Data berhasil ditambahkan');
+        $this->resetFields();
+    }
+
+    public function addData()
+    {
+        $this->isAddData = true;
+    }
+
+    public function resetFields()
+    {
+        $this->isAddData = false;
+
+        $this->nama = '';
+        $this->alamat = '';
     }
 }
--- resources/views/livewire/admin/master-data/customer.blade.php
+++ resources/views/livewire/admin/master-data/customer.blade.php
@@ -1,10 +1,39 @@
 <div>
-    <button class="btn btn-primary">Tambahkan Data</button>
+    <button wire:click="addData" class="btn btn-primary">Tambahkan Data</button>
+    @if (session('success'))     
+    <div class="alert alert-primary alert-dismissible fade show mt-2" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
 
+    @if ($isAddData)
+        <div wire:transition class="card my-3">
+            <div class="card-header">
+                <p class="fs-6">Tambahkan Data</p>
+            </div>
+            <div class="card-body">
+                <form wire:submit="store">
+                    <div class="my-2">
+                        <label for="Nama" class="form-label">Nama</label>
+                        <input wire:model="nama" type="text" class="form-control" placeholder="Masukkan Nama Pelanggan" required>
+                    </div>
+
+                    <div class="my-2">
+                        <label for="Alamat" class="form-label">Alamat</label>
+                        <textarea wire:model="alamat" class="form-control" rows="3" placeholder="Masukkan alamat...." required></textarea>
+                    </div>
+
+                    <button class="btn btn-primary mt-3" type="submit">Simpan</button>
+                </form>
+            </div>
+        </div>
+    @endif
     <div class="card mt-5">
         <div class="card-header">Data Customer</div>
         <div class="card-body">
-            <input wire:model.live="search" type="text" class="form-control" placeholder="Cari berdasarkan nama atau alamat....">
+            <input wire:model.live="search" type="text" class="form-control"
+                placeholder="Cari berdasarkan nama atau alamat....">
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -17,7 +46,7 @@
                 <tbody>
                     @foreach ($customers as $customer)
                         <tr wire:key="{{ $customer->id }}">
-                            <th  scope="row">{{ $loop->iteration }}</th>
+                            <th scope="row">{{ $loop->iteration }}</th>
                             <td>{{ $customer->nama }}</td>
                             <td>{{ $customer->alamat }}</td>
                             <td></td>
