ba5cd839baec80e3dc0052a80bb103c9f37e3c73
Bayu Febriyono Putra <92196679+BayuFebriyono@users.noreply.github.com>
2024-02-08 22:09:03 +0700

    tambah data pembelian (belum masuk ke DB)

--- app/Livewire/Admin/Pembelian/Invoice.php
+++ app/Livewire/Admin/Pembelian/Invoice.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Admin\Pembelian;
 
+use App\Models\Barang;
+use Exception;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -10,8 +12,125 @@ use Livewire\Component;
 #[Title('Invoice Pembelian')]
 class Invoice extends Component
 {
+    public $showForm = false;
+    public $dataPembelian;
+    public $aktualId = '';
+    public $remarkId = '';
+
+    public $namaBarang;
+    public $kodeBarang;
+    public $diskon;
+    public $qty;
+    public $harga;
+    public $aktual;
+    public $remark;
+
+    public function mount()
+    {
+        $this->dataPembelian = collect();
+    }
+
     public function render()
     {
         return view('livewire.admin.pembelian.invoice');
+    }
+
+    public function add()
+    {
+        $this->showForm = true;
+    }
+
+    public function addBarang()
+    {
+        $this->dataPembelian->push([
+            'id' => uniqid(),
+            'kode_barang' => $this->kodeBarang,
+            'nama_barang' => $this->namaBarang,
+            'qty' => $this->qty,
+            'aktual' => $this->qty,
+            'harga' => $this->harga,
+            'diskon' => $this->diskon,
+            'remark' => null,
+            'status' => 'WAITING'
+        ]);
+    }
+
+    public function cancel()
+    {
+        $this->showForm = false;
+    }
+
+    public function confirmed($id)
+    {
+        $this->dataPembelian->transform(function ($item, $key) use ($id) {
+            if ($item['id'] === $id) $item['status'] = 'CONFIRMED';
+
+            return $item;
+        });
+    }
+
+    public function confirmAll()
+    {
+        $this->dataPembelian->transform(function ($item, $key) {
+            $item['status'] = 'CONFIRMED';
+            return $item;
+        });
+    }
+
+    public function waiting($id)
+    {
+        $this->dataPembelian->transform(function ($item, $key) use ($id) {
+            if ($item['id'] === $id) $item['status'] = 'WAITING';
+            return $item;
+        });
+    }
+
+    public function showAktual($id)
+    {
+        $this->aktualId = $id;
+    }
+
+    public function updateAktual()
+    {
+        $this->dataPembelian->transform(function ($item, $key) {
+            if ($item['id'] === $this->aktualId) $item['aktual'] = $this->aktual;
+            return $item;
+        });
+
+        $this->aktualId = '';
+    }
+
+    public function cancelAktual()
+    {
+        $this->aktualId = '';
+    }
+
+    public function showRemark($id)
+    {
+        $this->remarkId = $id;
+    }
+
+    public function updateRemark()
+    {
+        $this->dataPembelian->transform(function ($item, $key) {
+            if ($item['id'] === $this->remarkId) $item['remark'] = $this->remark;
+            return $item;
+        });
+
+        $this->remarkId = '';
+    }
+
+    public function cancelRemark()
+    {
+        $this->remarkId = '';
+    }
+
+    public function searchBarang()
+    {
+        try {
+            $this->namaBarang = Barang::where('kode_barang', $this->kodeBarang)->first()->nama_barang;
+        } catch (Exception $e) {
+            $this->namaBarang = '';
+        }
     }
 }
--- resources/views/livewire/admin/pembelian/invoice.blade.php
+++ resources/views/livewire/admin/pembelian/invoice.blade.php
@@ -1,93 +1,161 @@
 <div class="container">
-    <div class="card">
-        <div class="card-body">
-            <p class="fs-5 fw-bold">Masukkan No Invoice anda</p>
-            <p class="text-muted">Menu dibawah ini dugunakan untuk mencari no invoice yang sudah ada</p>
-            <form class="mt-2">
-                <div class="row">
-                    <div class="col-md-10">
-                        <input type="text" class="form-control" placeholder="Masukkan No Invoice Yang Sudah Ada">
+    @if (!$showForm)
+        {{-- Cari Invoice --}}
+        <div class="card">
+            <div class="card-body">
+                <p class="fs-5 fw-bold">Masukkan No Invoice anda</p>
+                <p class="text-muted">Menu dibawah ini dugunakan untuk mencari no invoice yang sudah ada</p>
+                <form class="mt-2">
+                    <div class="row">
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" placeholder="Masukkan No Invoice Yang Sudah Ada">
+                        </div>
+                        <div class="col-md-2">
+                            <button type="submit" class="btn btn-inverse-primary">Cari</button>
+                        </div>
                     </div>
-                    <div class="col-md-2">
-                        <button type="submit" class="btn btn-inverse-primary">Cari</button>
+                </form>
+                <button wire:click='add' type="button" class="btn btn-md btn-inverse-success">Atau buat invoice
+                    baru</button>
+            </div>
+        </div>
+    @else
+        {{-- Form Pembelian --}}
+        <div class="card mt-3">
+            <div class="card-body">
+                <p class="fs-5 fw-bold">Form Pembelian Barang</p>
+                <div>
+
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <label for="kodeBarang" class="form-label">Kode Barang</label>
+                            <input wire:model='kodeBarang' wire:change='searchBarang' type="text"
+                                class="form-control" placeholder="Masukkan kode barang" required>
+                            <p class="fw-bold mt-1">Nama Barang : {{ $namaBarang }}</p>
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="Diskon" class="form-label">Diskon</label>
+                            <input wire:model='diskon' type="number" class="form-control" placeholder="Masukkan diskon"
+                                required>
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <label for="qty" class="form-label">Quantity</label>
+                            <input wire:model='qty' type="number" id="qty" class="form-control"
+                                placeholder="masukkan quantity dalam dus" required>
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="harga" class="form-label">Harga</label>
+                            <input wire:model='harga' type="number" id="harga" class="form-control"
+                                placeholder="Masukkan harga" required>
+                        </div>
+                    </div>
+                    <div class="mt-3">
+                        <button wire:click='addBarang' type="submit"
+                            class="btn btn-md btn-inverse-primary">Submit</button>
+                        <button wire:click='cancel' type="button"
+                            class="btn btn-md btn-inverse-danger ">Cancel</button>
                     </div>
                 </div>
-            </form>
-            <button type="button" class="btn btn-md btn-inverse-success">Atau buat invoice baru</button>
+
+            </div>
         </div>
-    </div>
 
-
-    {{-- Form Pembelian --}}
-    <div class="card mt-3">
-        <div class="card-body">
-            <p class="fs-5 fw-bold">Form Pembelian Barang</p>
-            <form action="">
-                <div class="row mt-3">
-                    <div class="col-md-6">
+        {{-- Table --}}
+        <div class="card mt-4">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <div class="p-2">
+                        <h4 class="card-title">Data konfirmasi pembelian</h4>
+                        <p class="my-2 text-muted">Data ini temporary tidak akan masuk ke database selama belum di save
+                        </p>
+                        <button wire:click='confirmAll' class="btn btn-md btn-inverse-warning">Confirm All</button>
+                    </div>
+                    <div class="ms-auto p-2">
                         <label for="Suplier" class="form-label">Pilih Suplier</label>
                         <input type="text" class="form-control mb-2" placeholder="Cari suplier...">
                         <select class="form-select" required>
                             <option value="">---Pilih Suplier---</option>
                         </select>
                     </div>
-
-                    <div class="col-md-6">
-                        <label for="kodeBarang" class="form-label">Kode Barang</label>
-                        <input type="text" class="form-control" placeholder="Masukkan kode barang" required>
-                        <p class="fw-bold mt-1">Nama Barang :</p>
+                    <div class="p-2">
+                        <button class="btn btn-ms btn-inverse-success" type="button">Simpan Invoice</button>
                     </div>
                 </div>
 
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <label for="qty" class="form-label">Quantity</label>
-                        <input type="number" id="qty" class="form-control" placeholder="masukkan quantity"
-                            required>
-                    </div>
+                {{-- Aktual --}}
+                @if ($aktualId)
+                        <div class="row my-3">
+                            <div class="col-md-8">
+                                <input wire:model='aktual' type="number" class="form-control " placeholder="Aktual...">
+                            </div>
+                            <div class="col-md-4 ">
+                                <button wire:click='updateAktual' type="button" class="btn btn-primary btn-sm">Save</button>
+                                <button wire:click='cancelAktual' type="button" class="btn btn-sm btn-secondary">Cancel</button>
+                            </div>
+                        </div>
+                @endif
+                {{-- Remark --}}
+                @if ($remarkId)
+                        <div class="row my-3">
+                            <div class="col-md-8">
+                                <input wire:model='remark' type="text" class="form-control " placeholder="Remark...">
+                            </div>
+                            <div class="col-md-4 ">
+                                <button wire:click='updateRemark' type="button" class="btn btn-primary btn-sm">Save</button>
+                                <button wire:click='cancelRemark' type="button" class="btn btn-sm btn-secondary">Cancel</button>
+                            </div>
+                        </div>
+                @endif
 
-                    <div class="col-md-6">
-                        <label for="harga" class="form-label">Harga</label>
-                        <input type="number" id="harga" class="form-control" placeholder="Masukkan harga" required>
-                    </div>
+                <div class="table-responsive mt-5">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Kode Barang</th>
+                                <th>Nama Barang</th>
+                                <th>Qty</th>
+                                <th>Aktual</th>
+                                <th>Harga</th>
+                                <th>Diskon</th>
+                                <th>Remarks</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($dataPembelian as $data)
+                                <tr wire:key='{{ $data['id'] }}'>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $data['kode_barang'] }}</td>
+                                    <td>{{ $data['nama_barang'] }}</td>
+                                    <td>{{ $data['qty'] }}</td>
+                                    <td wire:click='showAktual("{{ $data['id'] }}")' class="text-primary"
+                                        role="button"><u>{{ $data['aktual'] }}</u></td>
+                                    <td>{{ formatRupiah($data['harga']) }}</td>
+                                    <td>{{ $data['diskon'] }}</td>
+                                    <td wire:click='showRemark("{{ $data['id'] }}")' role="button" class="text-primary"><u>{{ $data['remark'] ?? '-' }}</u></td>
+                                    <td>
+                                        @if ($data['status'] == 'WAITING')
+                                            <span wire:click='confirmed("{{ $data['id'] }}")' role="button"
+                                                class="btn btn-sm btn-warning"><i
+                                                    class="mdi mdi-check-outline"></i></span>
+                                        @elseif ($data['status'] == 'CONFIRMED')
+                                            <span wire:click='waiting("{{ $data['id'] }}")' role="button"
+                                                class="btn btn-sm btn-success"><i
+                                                    class="mdi mdi-check-outline"></i></span>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <label for="Diskon" class="form-label">Diskon</label>
-                        <input type="number" class="form-control" placeholder="Masukkan diskon" required>
-                    </div>
-                </div>
-
-                <button type="submit" class="btn btn-md btn-inverse-primary mt-2">Submit</button>
-            </form>
-        </div>
-    </div>
-
-    {{-- Table --}}
-    <div class="card mt-4">
-        <div class="card-body">
-            <h4 class="card-title">Data konfirmasi pembelian</h4>
-            <p class="my-2 text-muted">Data ini temporary tidak akan masuk ke database selama belum di save</p>
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Kode Barang</th>
-                            <th>Nama Barang</th>
-                            <th>Qty</th>
-                            <th>Aktual</th>
-                            <th>Harga</th>
-                            <th>Diskon</th>
-                            <th>Remarks</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
-                </table>
             </div>
         </div>
-    </div>
+    @endif
 </div>
