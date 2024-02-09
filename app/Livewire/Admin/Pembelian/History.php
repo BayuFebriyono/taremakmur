555e81cf52ae9b1ba619d44b1b9111c4d788edff
Bayu Febriyono Putra <92196679+BayuFebriyono@users.noreply.github.com>
2024-02-09 09:19:03 +0700

    add TCPDF

--- app/Livewire/Admin/Pembelian/History.php
+++ app/Livewire/Admin/Pembelian/History.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin\Pembelian;
 
 use App\Models\HeaderPembelian;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -14,13 +15,22 @@ class History extends Component
 {
     use WithPagination;
     public $perPage = 10;
-    public $search ='';
+    public $search = '';
 
     public function render()
     {
         $pembelians = HeaderPembelian::with(['user', 'suplier'])->latest()->paginate($this->perPage);
-        return view('livewire.admin.pembelian.history',[
+        return view('livewire.admin.pembelian.history', [
             'pembelians' => $pembelians
         ]);
+    }
+
+    public function generateNota()
+    {
+        $pdf = Pdf::loadView('print.nota-pembelian')->setPaper('80mm', 'auto')->output();
+        return response()->streamDownload(
+            fn() => print($pdf),
+            'file_name.pdf'
+        );
     }
 }
---
+++ resources/views/print/nota-pembelian.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+       
+    </style>
+</head>
+
+<body>
+    <p style="text-align: center;">TARE MAKMUR<br>Jl. Gajah Mada Gg Semangka<br>Mojokerto</p>
+    <p>===============================================================</p>
+    <table style="width: 100%; border-collapse: collapse; border: none rgb(0, 0, 0);">
+        <tbody>
+            <tr>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">No Invoice</td>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">Datetime</td>
+            </tr>
+            <tr>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">Suplier</td>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">Nama Suplier</td>
+            </tr>
+        </tbody>
+    </table>
+    <p>-----------------------------------------------------------------------------------------------------------</p>
+    <p> 1) Gandum 2000 Box</p>
+    <table style="width: 100%;">
+        <tbody>
+            <tr>
+                <td style="width: 25.0000%;">Qty</td>
+                <td style="width: 25.0000%;">Harga Satuan</td>
+                <td style="width: 25.0000%;">Diskon</td>
+                <td style="width: 25.0000%;">Total</td>
+            </tr>
+        </tbody>
+    </table>
+    <p>------------------------------------------------------------------------------------------------------------</p>
+    <table style="width: 100%; border-collapse: collapse; border: none rgb(0, 0, 0);">
+        <tbody>
+            <tr>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">Total</td>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">
+                    <div style="text-align: right;">Rp 1.000.000</div>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">Pembayaran</td>
+                <td style="width: 50%; border: none rgb(0, 0, 0);">Tunai</td>
+            </tr>
+        </tbody>
+    </table>
+    <p>Keterangan :<br>dewdewdwedejd</p>
+</body>
+
+</html>
