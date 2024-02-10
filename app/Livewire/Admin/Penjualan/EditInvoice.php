be87041497bbc438d76c0647c6e5298ff1f94b7b
Bayu Febriyono Putra <92196679+BayuFebriyono@users.noreply.github.com>
2024-02-10 14:03:15 +0700

    add edit aktual and remark penjualan

--- app/Livewire/Admin/Penjualan/EditInvoice.php
+++ app/Livewire/Admin/Penjualan/EditInvoice.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Admin\Penjualan;
 
+use App\Models\DetailPembelian;
+use App\Models\DetailPenjualan;
 use App\Models\HeaderPenjualan;
 use Livewire\Component;
 
@@ -10,6 +12,8 @@ class EditInvoice extends Component
     public $remarkId = '';
     public $aktualId = '';
     public $noInvoice;
+    public $aktual = '';
+    public $remark = '';
 
     public function mount($noInvoice)
     {
@@ -22,6 +26,47 @@ class EditInvoice extends Component
             ->with(['detail_penjualan.barang', 'customer'])
             ->first();
         return view('livewire.admin.penjualan.edit-invoice', ['dataPenjualan' => $penjualans]);
+    }
+
+    public function showAktual($id)
+    {
+        $this->aktualId = $id;
+    }
+
+    public function updateAktual()
+    {
+        
+        DetailPenjualan::find($this->aktualId)
+            ->update([
+                'aktual' => $this->aktual
+            ]);
+        $this->aktualId = '';
+        $this->aktual = '';
+    }
+
+    public function updateRemark()
+    {
+        DetailPenjualan::find($this->remarkId)
+            ->update([
+                'remark' => $this->remark
+            ]);
+        $this->remarkId = '';
+        $this->remark = '';
+    }
+
+    public function cancelAktual()
+    {
+        $this->aktualId = '';
+    }
+
+    public function showRemark($id)
+    {
+        $this->remarkId = $id;
+    }
+
+    public function cancelRemark()
+    {
+        $this->remarkId = '';
     }
 
     public function cancel()
