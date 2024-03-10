5f3df4b5eb4fa1755a24c90d1d778b3b449628da
Bayu Febriyono Putra <92196679+BayuFebriyono@users.noreply.github.com>
2024-03-10 11:09:15 +0700

    Membuat import barang bisa update data barang

--- app/Livewire/Admin/Excel/BarangExcel.php
+++ app/Livewire/Admin/Excel/BarangExcel.php
@@ -38,20 +38,38 @@ class BarangExcel extends Component
             $data = $spreadsheet->getActiveSheet()->removeRow(1, 4)->toArray();
             foreach ($data as $row) {
                 $suplier = Suplier::select('id')->where('nama', $row[0])->first();
-              
-                Barang::create([
-                    'suplier_id' => $suplier->id,
-                    'kode_barang' => $row[1],
-                    'nama_barang' => $row[2],
-                    'cash_dus' => $row[3],
-                    'cash_pack' => $row[4],
-                    'harga_beli_dus' => $row[5],
-                    'harga_beli_pack' => $row[6],
-                    'kredit_dus' => $row[7],
-                    'kredit_pack' => $row[8],
-                    'diskon' => $row[9],
-                    'jumlah_renteng' => $row[10]
-                ]);
+
+                $barang = Barang::where('kode_barang', $row[1])->first();
+
+                if ($barang) {
+                    $barang->update([
+                        'suplier_id' => $suplier->id,
+                        'kode_barang' => $row[1],
+                        'nama_barang' => $row[2],
+                        'cash_dus' => $row[3],
+                        'cash_pack' => $row[4],
+                        'harga_beli_dus' => $row[5],
+                        'harga_beli_pack' => $row[6],
+                        'kredit_dus' => $row[7],
+                        'kredit_pack' => $row[8],
+                        'diskon' => $row[9],
+                        'jumlah_renteng' => $row[10]
+                    ]);
+                } else {
+                    Barang::create([
+                        'suplier_id' => $suplier->id,
+                        'kode_barang' => $row[1],
+                        'nama_barang' => $row[2],
+                        'cash_dus' => $row[3],
+                        'cash_pack' => $row[4],
+                        'harga_beli_dus' => $row[5],
+                        'harga_beli_pack' => $row[6],
+                        'kredit_dus' => $row[7],
+                        'kredit_pack' => $row[8],
+                        'diskon' => $row[9],
+                        'jumlah_renteng' => $row[10]
+                    ]);
+                }
             }
 
             $this->cancelExcel('Data berhasil diimport');
